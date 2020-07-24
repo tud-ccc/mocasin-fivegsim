02d31a5219ecb6417ee3b5e58a226c3d2b8a5042
Christian Menard <christian.menard@tu-dresden.de>
2020-07-24 11:56:41 +0200

    make sure that the extracted conf dir is properly cleaned up

--- hydra_plugins/fivegsim_searchpath_plugin/fivegsim_searchpath_plugin.py
+++ hydra_plugins/fivegsim_searchpath_plugin/fivegsim_searchpath_plugin.py
@@ -10,7 +10,17 @@ from hydra.plugins.search_path_plugin import SearchPathPlugin
 
 class FiveGSimSearchPathPlugin(SearchPathPlugin):
 
+    def __init__(self):
+        super().__init__()
+        # importlib returns a contextmanager that is intended for use in a with
+        # statement. Since we have no control over the lifetime of this plugin,
+        # we manually enter the context on __init__ and exit it on __del__
+        self.path_cm = importlib.resources.path('fivegsim', 'conf')
+        self.path = str(self.path_cm.__enter__())
+
     def manipulate_search_path(self, search_path):
-        path = str(importlib.resources.path('fivegsim', 'conf').__enter__())
         search_path.append(
-            provider="fivegsim-searchpath-plugin", path=path, anchor="main")
+            provider="fivegsim-searchpath-plugin", path=self.path, anchor="main")
+
+    def __del__(self):
+        self.path_cm.__exit__(None, None, None)
