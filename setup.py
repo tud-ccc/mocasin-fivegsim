8069c60c6635b47b90d2f7e3d1b8add72ab820af
Christian Menard <christian.menard@tu-dresden.de>
2020-07-24 15:24:55 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys
 
-requires = []
+requires = ['pykpn']
 if sys.version_info.minor < 7:
     requires.append("importlib-resources")
 
@@ -9,7 +9,7 @@ setup(
     name="fivegsim",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=requires + [],
-    setup_requires=['pykpn'],
+    install_requires=[],
+    setup_requires=requires,
     include_package_data=True,
 )
