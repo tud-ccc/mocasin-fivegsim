96386c7fe36be86785df016def27cb2ad507f064
Christian Menard <christian.menard@tu-dresden.de>
2020-08-19 09:38:06 +0200

    delete the old dummy app and rename the 5g app

--- fivegsim/simulation.py
+++ fivegsim/simulation.py
@@ -35,122 +35,6 @@ class params(object):
     proc_time = get_task_time(tgff_name, tgff_proc_path)
 
 
-class DummyGraph(KpnGraph):
-    """The KPN graph of a dummy application
-
-    This is intended to be refined into a 5G graph.
-
-    The dummy graph consists of three tasks:
-    source -> compute -> sink
-
-    For the final 5G graph it probably makes sense to parameterize this class
-    and build slightly different graphs for different parts in the 5G trace.
-    """
-
-    def __init__(self):
-        super().__init__("dummy")
-        # 3 processes
-        source = KpnProcess("source")
-        compute = KpnProcess("compute")
-        sink = KpnProcess("sink")
-        # and 2 channels (with a token size of 16 bytes each)
-        chan_src = KpnChannel("chan_src", 16)
-        chan_sink = KpnChannel("chan_sink", 16)
-        # connect the processes
-        source.connect_to_outgoing_channel(chan_src)
-        compute.connect_to_incomming_channel(chan_src)
-        compute.connect_to_outgoing_channel(chan_sink)
-        sink.connect_to_incomming_channel(chan_sink)
-        # register all processes and channels
-        self.add_process(source)
-        self.add_process(compute)
-        self.add_process(sink)
-        self.add_channel(chan_src)
-        self.add_channel(chan_sink)
-
-
-class DummyTraceGenerator(TraceGenerator):
-    """Generates traces for the dummy application
-
-    This is a bit clumsy to do by hand at the moment. We might improve this in
-    the near future.
-    """
-
-    def __init__(self):
-        # build a dictionary of all the traces
-        trace = {}
-
-        # trace for process source on core type ARM_CORTEX_A7
-        trace["source"] = {}
-        trace["source"]["ARM_CORTEX_A7"] = [
-            # Process 1500 cycles, then write 1 token to chan_src, and
-            # terminate
-            TraceSegment(process_cycles=1500, write_to_channel="chan_src",
-                         n_tokens=1, terminate=True)
-        ]
-        # trace for process source on core type ARM_CORTEX_A15
-        trace["source"]["ARM_CORTEX_A15"] = [
-            # Process 1000 cycles, then write 1 token to chan_src, and
-            # terminate
-            TraceSegment(process_cycles=1000, write_to_channel="chan_src",
-                         n_tokens=1, terminate=True)
-        ]
-
-        # trace for process compute on core type ARM_CORTEX_A7
-        trace["compute"] = {}
-        trace["compute"]["ARM_CORTEX_A7"] = [
-            # Read one token from channel chan_src
-            TraceSegment(process_cycles=0, read_from_channel="chan_src",
-                         n_tokens=1),
-            # Process 15,000,000 cycles, then write 1 token to chan_sink, and
-            # terminate
-            TraceSegment(process_cycles=15000000,
-                         write_to_channel="chan_sink", n_tokens=1,
-                         terminate=True)
-        ]
-        # trace for process compute on core type ARM_CORTEX_A15
-        trace["compute"]["ARM_CORTEX_A15"] = [
-            # Read one token from channel chan_src
-            TraceSegment(process_cycles=0, read_from_channel="chan_src",
-                         n_tokens=1),
-            # Process 10,000,000 cycles, then write 1 token to chan_sink, and
-            # terminate
-            TraceSegment(process_cycles=10000000,
-                         write_to_channel="chan_sink", n_tokens=1,
-                         terminate=True)
-        ]
-
-        # trace for process source on core type ARM_CORTEX_A7
-        trace["sink"] = {}
-        trace["sink"]["ARM_CORTEX_A7"] = [
-            # Read one token from channel chan_sink
-            TraceSegment(process_cycles=0, read_from_channel="chan_sink",
-                         n_tokens=1),
-            # Process 1500 cycles, then terminate
-            TraceSegment(process_cycles=1500, terminate=True)
-        ]
-        # trace for process source on core type ARM_CORTEX_A15
-        trace["sink"]["ARM_CORTEX_A15"] = [
-            # Read one token from channel chan_sink
-            TraceSegment(process_cycles=0, read_from_channel="chan_sink",
-                         n_tokens=1),
-            # Process 1000 cycles, then terminate
-            TraceSegment(process_cycles=1000, terminate=True)
-        ]
-
-        self.trace = trace
-
-        # we also need to keep track of the current position in the trace
-        self.trace_pos = {"source": 0, "compute": 0, "sink": 0}
-
-    def reset(self):
-        self.trace_pos = {"source": 0, "compute": 0, "sink": 0}
-
-    def next_segment(self, process_name, processor_type):
-        pos = self.trace_pos[process_name]
-        self.trace_pos[process_name] = pos + 1
-        return self.trace[process_name][processor_type][pos]
-    
 class FivegGraph(KpnGraph):
     """The KPN graph of a 5G application
     
@@ -159,7 +43,7 @@ class FivegGraph(KpnGraph):
     """
 
     def __init__(self):
-        super().__init__("dummy")
+        super().__init__("fiveg")
 
         # dictionary for processes
         pmicf = {}
@@ -644,7 +528,7 @@ class FiveGSimulation(BaseSimulation):
                 # create a new mapping
                 mapping = mapper.generate_mapping()
                 # instantiate the application
-                app = RuntimeKpnApplication(name=f"dummy{i}",
+                app = RuntimeKpnApplication(name=f"fiveg{i}",
                                             kpn_graph=kpn,
                                             mapping=mapping,
                                             trace_generator=trace,
