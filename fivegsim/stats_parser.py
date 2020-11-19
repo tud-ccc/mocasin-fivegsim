0f54f64a017ce7d22155ee03bd589f78c5957df5
Andres Goens <goens@goenshome>
2020-11-19 12:18:24 +0100

    added a plugin to the parser functionality to parse 5g stats

---
+++ fivegsim/stats_parser.py
@@ -0,0 +1,20 @@
+import os
+import csv
+
+from pykpn.tasks import parse_multirun
+
+def fivegsim_stats_parser(dir):
+    with open(os.path.join(dir, "stats.csv"), 'r') as f:
+        reader = csv.DictReader(f)
+        keys = reader.fieldnames
+        results = []
+        for row in reader:
+            results.append(row)
+    return results, keys
+
+def initialize_stats_parser():
+    parse_multirun._parsers.update(
+        {'5g_stats_parser': (
+        'fivegsim.stats_parser',
+        'fivegsim_stats_parser',
+        'Parses the statistics file from the 5G application.')})
