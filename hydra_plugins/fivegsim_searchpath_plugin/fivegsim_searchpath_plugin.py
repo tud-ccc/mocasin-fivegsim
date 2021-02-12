977e8768912c04e46a7d5faed13f9a90a94125b0
Christian Menard <christian.menard@tu-dresden.de>
2021-02-12 10:47:34 +0100

    load example files directly from the package

--- hydra_plugins/fivegsim_searchpath_plugin/fivegsim_searchpath_plugin.py
+++ hydra_plugins/fivegsim_searchpath_plugin/fivegsim_searchpath_plugin.py
@@ -3,10 +3,24 @@
 #
 # Authors: Christian Menard
 
+import os
+import fivegsim
+
 from hydra.plugins.search_path_plugin import SearchPathPlugin
+from omegaconf import OmegaConf
 
 
 class FiveGSimSearchPathPlugin(SearchPathPlugin):
+    def __init__(self):
+        # register a custom resolver to resolve paths to files located within
+        # the fivegsim package
+        OmegaConf.register_resolver(
+            "fivegsim_path",
+            lambda path="": os.path.join(
+                os.path.dirname(fivegsim.__file__), path
+            ),
+        )
+
     def manipulate_search_path(self, search_path):
         search_path.append(
             provider="fivegsim",
