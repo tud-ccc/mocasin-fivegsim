86e93dcfd9e32d84515392cfa1ea290fc32c7e51
Julian Robledo Mejia <julian.robledo_mejia@tu-dresden.de>
2021-03-02 14:32:24 +0100

    Refactored "_manager_process" function, split into smaller functions

--- fivegsim/simulation.py
+++ fivegsim/simulation.py
@@ -73,20 +73,125 @@ class FiveGSimulation(BaseSimulation):
         platform = hydra.utils.instantiate(cfg["platform"])
         return FiveGSimulation(platform, cfg, **kwargs)
 
-    def _manager_process(self):
+    def _generate_graphs(self, nsubframe):
+        graphs = []
+        for ntrace in nsubframe.trace:
+            # create a new graph
+            graphs.append(FivegGraph(self.graph_cnt, ntrace))
+            self.graph_cnt += 1
+        return graphs
+
+    def _generate_traces(self, nsubframe, proc_time):
+        traces = []
+        for ntrace in nsubframe.trace:
+            # create a new graph
+            traces.append(FivegTrace(ntrace, proc_time))
+        return traces
+
+    def _merge_graphs_and_traces(self, copy_graphs, traces):
+        # create a combined trace object
+        prefixes = [f"{g.name}_" for g in copy_graphs]
+        sf_trace = MergedFivegTrace(prefixes, traces)
+        return sf_trace
+
+    def _graph_deepcopy(self, name, graphs):
+        sf_graph = DataflowGraph(name)
+        # work with a deepcopy of graphs so we don't need to be
+        # afraid of breaking anything in the existing data structures and
+        # can still use them later
+        copy_graphs = copy.deepcopy(graphs)
+        for graph in copy_graphs:
+            # add all processes and channels to one large graph
+            for p in graph.processes():
+                p.name = f"{graph.name}_{p.name}"
+                sf_graph.add_process(p)
+            for c in graph.channels():
+                c.name = f"{graph.name}_{c.name}"
+                sf_graph.add_channel(c)
+        return sf_graph
+
+    def _generate_mapper(self, sf_graph, sf_trace, graphs):
+         # create a new mapper (this should be TETRiS in the future) Note
+        # that we need to create a new mapper here, as the GRAPH could change
+        # This appears to be a weakness of our mapper interface. The GRAPH
+        # should probably become a parameter of generate_mapping().
+        log.info(f"generate mapping for {sf_graph.name}")
+        rep = hydra.utils.instantiate(
+            self.cfg["representation"], sf_graph, self.platform
+        )
+        mapper = hydra.utils.instantiate(
+            self.cfg["mapper"], sf_graph, self.platform, sf_trace, rep
+        )
+        # create a mapping for the entire subframe
+        sf_mapping = (
+            mapper.generate_mapping()
+        )  # TODO: collect and add load here
+        log.info(f"mapping generation done")
+
+        # split the mapping up again
+        mappings = []
+        for graph in graphs:
+            mapping = Mapping(graph, self.platform)
+            for sf_p in sf_mapping._process_info.keys():
+                if sf_p.startswith(graph.name):
+                    p = sf_p[len(graph.name) + 1 :]
+                    mapping._process_info[p] = sf_mapping._process_info[
+                        sf_p
+                    ]
+            for sf_c in sf_mapping._channel_info.keys():
+                if sf_c.startswith(graph.name):
+                    c = sf_c[len(graph.name) + 1 :]
+                    mapping._channel_info[c] = sf_mapping._channel_info[
+                        sf_c
+                    ]
+            mappings.append(mapping)
+
+        log.info(f"start application {sf_graph.name}")
+        return mappings
+
+    def _simulate(self, mappings, traces, nsubframe):
+        cnt = 0
         trace_writer = self.system.trace_writer
 
-        app_finished = []
-        criticalities = []
-        prbs = []
-        mod = []
+        for mapping, trace in zip(mappings, traces):
+            criticality = nsubframe.trace[cnt].UE_criticality
+            prbs = nsubframe.trace[cnt].PRBs
+            mod = nsubframe.trace[cnt].modulation_scheme
+
+            # instantiate the application
+            app = FiveGRuntimeDataflowApplication(
+                name=mapping.graph.name,
+                graph=mapping.graph,
+                mapping=mapping,
+                app_trace=trace,
+                system=self.system,
+            )
+            # record application start in the simulation trace
+            trace_writer.begin_duration("instances", app.name, app.name)
+            # start the application
+            finished = self.env.process(
+                app.run(criticality, prbs, mod)
+            )
+            cnt += 1
+            # register a callback to record the application termination
+            # in the simulation trace
+            finished.callbacks.append(
+                lambda _, name=app.name: trace_writer.end_duration(
+                    "instances", name, name
+                )
+            )
+            # keep the finished event for later
+            self.app_finished.append(finished)
+
+    def _manager_process(self):
+        self.app_finished = []
+        sf_count = 0
+        self.graph_cnt = 0
+
         pStatFile = open("stats.csv", "w")
         pStatFile.write("startTime,endTime,criticality,miss,prbs,mod" + "\n")
         pStatFile.close()
 
-        i = 0
-        cnt = 0
-        sf_count = 0
         # while end of file not reached:
         while self.TFM.TF_EOF is not True:
 
@@ -94,18 +199,8 @@ class FiveGSimulation(BaseSimulation):
             sf_count += 1
 
             # run 100 instances of the 5G app, start one every 1 ms
-            graphs = []
-            traces = []
-            for ntrace in nsubframe.trace:
-                # create a new graph and trace
-                self.ntrace = ntrace
-
-                graphs.append(FivegGraph(i, self.ntrace))
-                traces.append(FivegTrace(self.ntrace, self.proc_time))
-                i += 1
-                criticalities.append(ntrace.UE_criticality)
-                prbs.append(ntrace.PRBs)
-                mod.append(ntrace.modulation_scheme)
+            graphs = self._generate_graphs(nsubframe)
+            traces = self._generate_traces(nsubframe, self.proc_time)
 
             # just wait and try again if there is nothing to process
             if len(graphs) == 0:
@@ -116,92 +211,20 @@ class FiveGSimulation(BaseSimulation):
             # XXX Merge the applications and traces generated above into one
             # large graph and trace for the entire subframe. This is just a
             # workaround for our mapper API that only accepts a single mapping
-            sf_graph = DataflowGraph(name=f"sf_{sf_count}")
-            # work with a deepcopy of graphs so we don't need to be
-            # afraid of breaking anything in the existing data structures and
-            # can still use them later
-            copy_graphs = copy.deepcopy(graphs)
-            for graph in copy_graphs:
-                # add all processes and channels to one large graph
-                for p in graph.processes():
-                    p.name = f"{graph.name}_{p.name}"
-                    sf_graph.add_process(p)
-                for c in graph.channels():
-                    c.name = f"{graph.name}_{c.name}"
-                    sf_graph.add_channel(c)
+            sf_trace = self._merge_graphs_and_traces(graphs, traces)
 
-            # create a combined trace object
-            prefixes = [f"{g.name}_" for g in copy_graphs]
-            sf_trace = MergedFivegTrace(prefixes, traces)
+            # generate mappers
+            sf_graph = self._graph_deepcopy(f"sf_{sf_count}", graphs)
+            mappings = self._generate_mapper(sf_graph, sf_trace, graphs)
 
-            # create a new mapper (this should be TETRiS in the future) Note
-            # that we need to create a new mapper here, as the GRAPH could change
-            # This appears to be a weakness of our mapper interface. The GRAPH
-            # should probably become a parameter of generate_mapping().
-            log.info(f"generate mapping for {sf_graph.name}")
-            rep = hydra.utils.instantiate(
-                self.cfg["representation"], sf_graph, self.platform
-            )
-            mapper = hydra.utils.instantiate(
-                self.cfg["mapper"], sf_graph, self.platform, sf_trace, rep
-            )
-            # create a mapping for the entire subframe
-            sf_mapping = (
-                mapper.generate_mapping()
-            )  # TODO: collect and add load here
-            log.info(f"mapping generation done")
-
-            # split the mapping up again
-            mappings = []
-            for graph in graphs:
-                mapping = Mapping(graph, self.platform)
-                for sf_p in sf_mapping._process_info.keys():
-                    if sf_p.startswith(graph.name):
-                        p = sf_p[len(graph.name) + 1 :]
-                        mapping._process_info[p] = sf_mapping._process_info[
-                            sf_p
-                        ]
-                for sf_c in sf_mapping._channel_info.keys():
-                    if sf_c.startswith(graph.name):
-                        c = sf_c[len(graph.name) + 1 :]
-                        mapping._channel_info[c] = sf_mapping._channel_info[
-                            sf_c
-                        ]
-                mappings.append(mapping)
-
-            log.info(f"start application {sf_graph.name}")
             # simulate the actual applications
-            for mapping, trace in zip(mappings, traces):
-                # instantiate the application
-                app = FiveGRuntimeDataflowApplication(
-                    name=mapping.graph.name,
-                    graph=mapping.graph,
-                    mapping=mapping,
-                    app_trace=trace,
-                    system=self.system,
-                )
-                # record application start in the simulation trace
-                trace_writer.begin_duration("instances", app.name, app.name)
-                # start the application
-                finished = self.env.process(
-                    app.run(criticalities[cnt], prbs[cnt], mod[cnt])
-                )
-                cnt += 1
-                # register a callback to record the application termination
-                # in the simulation trace
-                finished.callbacks.append(
-                    lambda _, name=app.name: trace_writer.end_duration(
-                        "instances", name, name
-                    )
-                )
-                # keep the finished event for later
-                app_finished.append(finished)
+            self._simulate(mappings, traces, nsubframe)
 
             # wait for 1 ms
             yield self.env.timeout(1000000000)
 
         # wait until all applications finished
-        yield self.env.all_of(app_finished)
+        yield self.env.all_of(self.app_finished)
 
         print("missrate = " + str(self.get_missrate()))
 
