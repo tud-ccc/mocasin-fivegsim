8e7a3d5eabd921d1796f012f1e66f6ff3252e88b
Julian Robledo Mejia <julian.robledo_mejia@tu-dresden.de>
2021-03-04 08:33:33 +0100

    Added calculation of energy consumption to simulation

--- fivegsim/simulation.py
+++ fivegsim/simulation.py
@@ -244,6 +244,11 @@ class FiveGSimulation(BaseSimulation):
         # save the execution time
         self.exec_time = self.env.now
 
+        if self.system.power_enabled:
+            energy_results = self.system.calculate_energy()
+            (self.static_energy, self.dynamic_energy) = energy_results
+            self.total_energy = self.static_energy + self.dynamic_energy
+
     def get_missrate(self):
         pStatFile = open("stats.csv", "r")
         lines = pStatFile.readlines()  # Load all lines
