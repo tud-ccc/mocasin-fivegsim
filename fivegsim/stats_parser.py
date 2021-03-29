067a3bfe09004faedd2643ea2a4e72f7597777a2
Julian Robledo Mejia <julian.robledo_mejia@tu-dresden.de>
2021-03-29 21:54:36 +0200

    Added parser for missrate output file

--- fivegsim/stats_parser.py
+++ fivegsim/stats_parser.py
@@ -14,6 +14,24 @@ def fivegsim_stats_parser(dir):
     return results, keys
 
 
+def fiveg_missrate_parser(dir):
+    results = {}
+    try:
+        with open(os.path.join(dir, "missrate.txt"), "r") as f:
+            results["total_apps"] = int(
+                f.readline().replace("Total applications: ", "")
+            )
+            results["apps_rejected"] = int(
+                f.readline().replace("Total rejected: ", "")
+            )
+            results["missed_deadlines"] = int(
+                f.readline().replace("Missed deadline: ", "")
+            )
+        return results, list(results.keys())
+    except FileNotFoundError:
+        return {}, []
+
+
 def initialize_stats_parser():
     parse_multirun._parsers.update(
         {
@@ -21,6 +39,11 @@ def initialize_stats_parser():
                 "fivegsim.stats_parser",
                 "fivegsim_stats_parser",
                 "Parses the statistics file from the 5G application.",
-            )
+            ),
+            "5g_missrate_parser": (
+                "fivegsim.stats_parser",
+                "fiveg_missrate_parser",
+                "Parses the number of missed applications from a file",
+            ),
         }
     )
