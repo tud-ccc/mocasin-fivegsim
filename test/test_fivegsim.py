1c14de60d0fbe77654a622ef09e5ac296fe5fb00
Robert Khasanov <robert.khasanov@tu-dresden.de>
2021-08-28 01:11:20 +0200

    test: added new test for fivegsim, rewritten test_fivegsim.py

--- test/test_fivegsim.py
+++ test/test_fivegsim.py
@@ -3,120 +3,68 @@
 #
 # Authors: Christian Menard
 
-import os
+from pathlib import Path
+import pytest
 import subprocess
 
 
-def test_fivegsim(tmpdir):
-    trace_file = os.path.join(os.path.dirname(__file__), "test_trace.txt")
+@pytest.mark.parametrize(
+    "trace,platform,runtime,total,rejected,missed,simulated",
+    [
+        ("lte_trace_1.txt", "odroid", "None", 18, 0, 2, 31.0),
+        ("lte_trace_2.txt", "odroid", "None", 18, 0, 12, 11.0),
+        ("lte_trace_2.txt", "odroid_acc", "None", 18, 0, 12, 11.0),
+        ("lte_trace_1.txt", "odroid_acc", "load_balancer", 18, 0, 2, 31.0),
+        ("lte_trace_2.txt", "odroid", "load_balancer", 18, 0, 10, 11.0),
+        ("lte_trace_2.txt", "odroid_acc", "load_balancer", 18, 0, 5, 11.0),
+        ("lte_trace_1.txt", "odroid_acc", "tetris", 18, 2, 0, 31.0),
+        ("lte_trace_2.txt", "odroid", "tetris", 18, 7, 0, 11.36),
+        ("lte_trace_2.txt", "odroid_acc", "tetris", 18, 6, 0, 11.40),
+    ],
+)
+def test_fivegsim(
+    tmpdir, trace, total, platform, runtime, rejected, missed, simulated
+):
+    trace_file = Path(__file__).parent.resolve().joinpath(trace)
 
-    res = subprocess.run(
-        [
-            "fivegsim",
-            f"trace_file={trace_file}",
-            "platform=odroid",
-            "platform.processor_0.type=ARM_CORTEX_A7",
-            "platform.processor_1.type=ARM_CORTEX_A15",
-        ],
-        cwd=tmpdir,
-        check=True,
-        stdout=subprocess.PIPE,
-    )
-    found_lines = 0x0
-    stdout = res.stdout.decode()
-    for line in stdout.split("\n"):
-        if line.startswith("Total applications: "):
-            total = line[20:]
-            assert total == "18"
-            found_lines |= 0x1
-        if line.startswith("Total rejected: "):
-            missed = line[16:]
-            assert missed == "0"
-            found_lines |= 0x2
-        if line.startswith("Missed deadline: "):
-            missed = line[17:]
-            assert missed == "2"
-            found_lines |= 0x4
-        if line.startswith("Total simulated time: "):
-            time = line[22:]
-            assert time == "31.0 ms"
-            found_lines |= 0x8
+    cmd = ["fivegsim", f"trace_file={trace_file}", f"platform={platform}"]
+    if platform == "odroid":
+        cmd.append("platform.processor_0.type=ARM_CORTEX_A7")
+        cmd.append("platform.processor_1.type=ARM_CORTEX_A15")
+    if runtime == "load_balancer":
+        cmd.append("load_balancer=true")
+    elif runtime == "tetris":
+        cmd.append("mapper=fiveg")
+        cmd.append("tetris_runtime=true")
+        if platform == "odroid":
+            cmd.append("pareto_time_scale=1.09")
+            cmd.append("pareto_time_offset=0.10")
+        elif platform == "odroid_acc":
+            cmd.append("pareto_time_scale=1.05")
+            cmd.append("pareto_time_offset=0.24")
+    else:
+        assert runtime == "None"
 
-    assert found_lines == 0xF
-
-
-def test_fivegsim_with_load_balancer(tmpdir):
-    trace_file = os.path.join(os.path.dirname(__file__), "test_trace.txt")
-
-    res = subprocess.run(
-        [
-            "fivegsim",
-            f"trace_file={trace_file}",
-            "load_balancer=true",
-            "platform=odroid_acc",
-        ],
-        cwd=tmpdir,
-        check=True,
-        stdout=subprocess.PIPE,
-    )
+    res = subprocess.run(cmd, cwd=tmpdir, check=True, stdout=subprocess.PIPE)
 
     found_lines = 0x0
     stdout = res.stdout.decode()
     for line in stdout.split("\n"):
         if line.startswith("Total applications: "):
-            total = line[20:]
-            assert total == "18"
+            test_total = int(line[20:])
+            assert test_total == total
             found_lines |= 0x1
         if line.startswith("Total rejected: "):
-            missed = line[16:]
-            assert missed == "0"
+            test_rejected = int(line[16:])
+            assert test_rejected == rejected
             found_lines |= 0x2
         if line.startswith("Missed deadline: "):
-            missed = line[17:]
-            assert missed == "2"
+            test_missed = int(line[17:])
+            assert test_missed == missed
             found_lines |= 0x4
         if line.startswith("Total simulated time: "):
-            time = line[22:]
-            assert time == "31.0 ms"
-            found_lines |= 0x8
-
-    assert found_lines == 0xF
-
-
-def test_fivegsim_with_tetris(tmpdir):
-    trace_file = os.path.join(os.path.dirname(__file__), "test_trace.txt")
-
-    res = subprocess.run(
-        [
-            "fivegsim",
-            f"trace_file={trace_file}",
-            "tetris_runtime=true",
-            "mapper=fiveg",
-            "platform=odroid_acc",
-        ],
-        cwd=tmpdir,
-        check=True,
-        stdout=subprocess.PIPE,
-    )
-
-    found_lines = 0x0
-    stdout = res.stdout.decode()
-    for line in stdout.split("\n"):
-        if line.startswith("Total applications: "):
-            total = line[20:]
-            assert total == "18"
-            found_lines |= 0x1
-        if line.startswith("Total rejected: "):
-            missed = line[16:]
-            assert missed == "1"
-            found_lines |= 0x2
-        if line.startswith("Missed deadline: "):
-            missed = line[17:]
-            assert missed == "4"
-            found_lines |= 0x4
-        if line.startswith("Total simulated time: "):
-            time = line[22:]
-            assert time == "31.0 ms"
+            test_simulated = float(line[22:].split()[0])
+            assert abs(test_simulated - simulated) < 0.01
             found_lines |= 0x8
 
     assert found_lines == 0xF
